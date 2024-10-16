a4865535a66e195de0586c0db3ab5a242ad60613
CheeseMcBaguette <29225021+cheesemcbaguette@users.noreply.github.com>
2024-10-16 11:44:03 +0200

    Serve index.html when accessing the server

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	serveMux := http.NewServeMux()
 
+	// serve current directory when accessing the root of the server
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := &http.Server{
 		Addr:    ":8080",  // Bind to localhost:8080
 		Handler: serveMux, // Use the ServeMux as the handler
